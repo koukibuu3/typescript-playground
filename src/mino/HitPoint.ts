f68c77aa54658932fa5835d338eb86672a20e9c7
koukibuu3 <koukibuu3@gmail.com>
2022-07-01 23:55:42 +0900

    feat: add HitPoint Class

---
+++ src/mino/HitPoint.ts
@@ -0,0 +1,36 @@
+/** ヒットポイント（HP）を表現するクラス */
+class HitPoint {
+  /** 最小ヒットポイント */
+  private static readonly MIN = 0
+
+  /** 最大ヒットポイント */
+  private static readonly MAX = 100
+
+  /** 現在ヒットポイント */
+  public value: number
+
+  constructor(value: number) {
+    if (value < HitPoint.MIN)
+      throw new Error(`${HitPoint.MIN} 以上を指定してください`)
+    if (value > HitPoint.MAX)
+      throw new Error(`${HitPoint.MAX} 以下を指定してください`)
+
+    this.value = value
+  }
+
+  /** ダメージを受ける */
+  damage(damageAmount: number): HitPoint {
+    const damaged = this.value - damageAmount
+    const corrected = damaged < HitPoint.MIN ? HitPoint.MIN : damaged
+    return new HitPoint(corrected)
+  }
+
+  /** 回復する */
+  recover(recoverAmount: number): HitPoint {
+    const recovered = this.value + recoverAmount
+    const corrected = HitPoint.MAX < recovered ? HitPoint.MAX : recovered
+    return new HitPoint(corrected)
+  }
+}
+
+export default HitPoint
